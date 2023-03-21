f0e8b865f4b244deca0213a878d9b2aab1a17106
Angelito10 <92886923+Angelito10@users.noreply.github.com>
2023-03-20 18:48:57 -0600

    cambios

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config(object):
     SECRET_KEY = 'MY_SECRET_KEY'
     SESSION_COOKIE_SECURE = False
 
+
 class DevelopmetConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1/idgs801'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://1234:root@127.0.0.1/idgs801'
     SQLALCHEMY_TRACK_MODIFICATION = False
